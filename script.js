434b8ebc25e8f1349c57ae49975ddbae3c51bc1b
kdglauvygiadn <dklugan@yahoo.fr>
2022-08-30 18:54:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,327 @@
+
+        const quizData = [
+    {
+        Num:"1",
+    question: "Elles mangent………………pain",
+    a: "de la",
+    b: "la",
+    c: "du",
+    d: "une",
+    correct: "c",
+    },
+    {
+        Num:"2",
+    question: "Nous \xE9crivons………………tableau.",
+    a: "à la",
+    b: "aux",
+    c: "à l'",
+    d: "au",
+    correct: "d",
+    },
+    {
+        Num:"3",
+    question: "Kofi va chercher………………chaise",
+    a: "du",
+    b: "une",
+    c: "de la",
+    d: "des",
+    correct: "b",
+    },
+    {
+        Num:"4",
+    question: "Maman ach\xE8te………………tomates.",
+    a: "une",
+    b: "de",
+    c: "de la",
+    d: "des",
+    correct: "d",
+    },
+    {
+        Num:"5",
+    question: ". …………………….. enfant dort sur la table",
+    a: "Les",
+    b: "La",
+    c: "L'",
+    d: "De la",
+    correct: "c",
+    },
+    {
+        Num:"6",
+    question: "Le chat mange …………… L\xE9zards.",
+    a: "une",
+    b: "les",
+    c: "la",
+    d: "un",
+    correct: "b",
+    },
+    {
+        Num:"7",
+    question: "Ama \xE9coute …………le\xE7on.",
+    a: "le",
+    b: "à la",
+    c: "la",
+    d: "les",
+    correct: "c",
+    },
+    {
+        Num:"8",
+    question: "Donne-moi de………….eau.",
+    a: "une",
+    b: "un",
+    c: "la",
+    d: "l'",
+    correct: "d",
+    },
+    {
+        Num:"9",
+    question: ". ……………homme arrive",
+    a: "La",
+    b: "Les",
+    c: "Un",
+    d: "Une",
+    correct: "c",
+    },
+    {
+        Num:"10",
+    question: "Nous avons …………… belle maison.",
+    a: "une",
+    b: "un",
+    c: "le",
+    d: "les",
+    correct: "a",
+    },
+    {
+        Num:"11",
+    question: "Viens me ………..bonjour",
+    a: "saluer",
+    b: "parler",
+    c: "dire",
+    d: "voir",
+    correct: "c",
+    },
+    {
+        Num:"12",
+    question: " Aujourd\’hui c\’est samedi, et demain sera………….",
+    a: "dimanche",
+    b: "lundi",
+    c: "jeudi",
+    d: "vendredi",
+    correct: "a",
+    },
+    {
+        Num:"13",
+    question: "L\’homme vend des chemises et des robes. C\’est un………….",
+    a: "vendeur",
+    b: "voleur",
+    c: "professeur",
+    d: "pêcheur",
+    correct: "a",
+    },
+    {
+        Num:"14",
+    question: "Quel \xE2ge as-tu ? ……………….",
+    a: "J'ai froid",
+    b: "Je vais bien",
+    c: "Je mange bien",
+    d: "J'ai dix ans",
+    correct: "d",
+    },
+    {
+        Num:"15",
+    question: "Elle est malade. Elle prend……………",
+    a: "de la bi\xE8re",
+    b: "de l'aspirine",
+    c: "de vin",
+    d: "de l'eau",
+    correct: "b",
+    },
+    {
+        Num:"16",
+    question: " J'ai faim. Je veux……………..",
+    a: "dormir",
+    b: "parler",
+    c: "manger",
+    d: "courir",
+    correct: "c",
+    },
+    {
+        Num:"17",
+    question: "Le cocotier est …………",
+    a: "un arbre",
+    b: "un animal",
+    c: "un insecte",
+    d: "un fruit",
+    correct: "a",
+    },
+    {
+        Num:"18",
+    question: ". Le professeur corrige les……………………",
+    a: "classes",
+    b: "tableaux",
+    c: "devoirs",
+    d: "bancs",
+    correct: "c",
+    },
+    {
+        Num:"19",
+    question: " Le ………….. se trouve dans l'eau.",
+    a: "mouton",
+    b: "poisson",
+    c: "chat",
+    d: "chien",
+    correct: "b",
+    },
+    {
+        Num:"20",
+    question: "L'orange est …………",
+    a: "un b\xE2ton",
+    b: "une poup\xE9e",
+    c: "un journal",
+    d: "un fruit",
+    correct: "d",
+    },
+    {
+        Num:"21",
+    question: " Les \xE9l\xE8ves entrent ……….classe",
+    a: "au",
+    b: "à",
+    c: "en",
+    d: "pour",
+    correct: "c",
+    },
+    {
+        Num:"22",
+    question: "Le livre est …………… la table",
+    a: "dans",
+    b: "sur",
+    c: "\xE1",
+    d: "sans",
+    correct: "b",
+    },
+    {
+        Num:"23",
+    question: "Le voleur passe ……… la fen\xEAtre",
+    a: "sous",
+    b: "sans",
+    c: "dans",
+    d: "par",
+    correct: "d",
+    },
+    {
+        Num:"24",
+    question: " Cette dame porte des lunettes ……….bien voir",
+    a: "de",
+    b: "pour",
+    c: "en",
+    d: "par",
+    correct: "b",
+    },
+    {
+        Num:"25",
+    question: " L'enfant marche ………….. sa m\xE8re.",
+    a: "devant",
+    b: "pour",
+    c: "sous",
+    d: "sur",
+    correct: "a",
+    },
+    {
+        Num:"26",
+    question: " Ils vont…………… Lom\xE9 en autocar",
+    a: "sur",
+    b: "au",
+    c: "\xE1",
+    d: "du",
+    correct: "c",
+    },
+    {
+        Num:"27",
+    question: "Qui est …………… la porte ?",
+    a: "sur",
+    b: "derri\xE8re",
+    c: "sous",
+    d: "vers",
+    correct: "b",
+    },
+    {
+        Num:"28",
+    question: "Kofi va ………… France",
+    a: "de",
+    b: "vers",
+    c: "en",
+    d: "\xE1",
+    correct: "c",
+    },
+    {
+        Num:"29",
+    question: "Le chat …………..sur le lit",
+    a: "dort",
+    b: "va",
+    c: "regarde",
+    d: "\xE9crit",
+    correct: "a",
+    },
+    {
+        Num:"30",
+    question: "Maman donne des bonbons………………enfants",
+    a: "des",
+    b: "pour",
+    c: "au",
+    d: "aux",
+    correct: "d",
+    },
+    ];
+    const quiz = document.getElementById("quiz");
+    const answerElements = document.querySelectorAll(".answer");
+    const questionElement = document.getElementById("question");
+    const a_text = document.getElementById("a_text");
+    const b_text = document.getElementById("b_text");
+    const c_text = document.getElementById("c_text");
+    const d_text = document.getElementById("d_text");
+    const questionNumber = document.getElementById("questionNumber");
+    const submitButton = document.getElementById("submit");
+    let currentQuiz = 0;
+    let score = 0;
+
+    const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+    };
+    const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+    if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+    };
+    const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+    questionNumber.innerText = currentQuizData.Num;
+    };
+    loadQuiz();
+    submitButton.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+    if (answer === quizData[currentQuiz].correct) score++;
+    currentQuiz++;
+    if (currentQuiz < quizData.length) loadQuiz();
+    else {
+    quiz.innerHTML = `
+    <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+    <button onclick="history.go(0)">Play Again</button>
+    ` 
+    //location.reload(true);
+    setTimeout(function () { location.reload(true); }, 50000);
+    }
+
+    }
+    });
+   
+
+
+
